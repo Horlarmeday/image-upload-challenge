1e7459f68f2ed4350e274df4c08aebe4bf9169f0
Mahmud <ajaomahmud@gmail.com>
2021-04-25 18:06:13 +0100

    feat(Entity): Add Thumbnail entity

---
+++ src/modules/images/entities/thumbnail.entity.ts
@@ -0,0 +1,29 @@
+import { BelongsTo, Column, DataType, Default, ForeignKey, Model, PrimaryKey, Table } from "sequelize-typescript";
+import { Image } from "./image.entity";
+
+@Table
+export class ThumbnailEntity extends Model{
+  @Default(DataType.UUIDV4)
+  @PrimaryKey
+  @Column({
+    type: DataType.UUID,
+    allowNull: false,
+  })
+  id: string;
+
+  @Column({
+    type: DataType.TEXT,
+    allowNull: false
+  })
+  image_url: string;
+
+  @ForeignKey(() => Image)
+  @Column({
+    type: DataType.UUID,
+    allowNull: false,
+  })
+  image_id: string;
+
+  @BelongsTo(() => Image)
+  image: Image;
+}
