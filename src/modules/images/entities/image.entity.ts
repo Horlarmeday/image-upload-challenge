4ad3fc9cee1be2df8bb091e2324e48685c4ed03f
Mahmud <ajaomahmud@gmail.com>
2021-04-25 18:05:56 +0100

    feat(Entity): Add Image entity

--- src/modules/images/entities/image.entity.ts
+++ src/modules/images/entities/image.entity.ts
@@ -1,6 +1,28 @@
-import {Table, Column, Model} from 'sequelize-typescript'
+import { Table, Column, Model, PrimaryKey, DataType, Default, HasMany } from "sequelize-typescript";
+import { ThumbnailEntity } from "./thumbnail.entity";
 
 @Table
 export class Image extends Model {
+  @Default(DataType.UUIDV4)
+  @PrimaryKey
+  @Column({
+    type: DataType.UUID,
+    allowNull: false,
+  })
+  id: string;
 
+  @Column({
+    type: DataType.STRING,
+    allowNull: false
+  })
+  name: string;
+
+  @Column({
+    type: DataType.TEXT,
+    allowNull: false
+  })
+  image_url: string;
+
+  @HasMany(()=> ThumbnailEntity)
+  thumbnails: ThumbnailEntity
 }
