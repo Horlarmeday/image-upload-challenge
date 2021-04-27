bdca963239c6a3088de73861c156d9023efbfc34
Mahmud <ajaomahmud@gmail.com>
2021-04-27 12:52:50 +0100

    refactor: refactored server.ts

--- src/core/startup/server.ts
+++ src/core/startup/server.ts
@@ -1,23 +1,24 @@
 import '../config/env';
+import './logger'
 import * as express from 'express';
 import * as path from 'path';
 import * as fileUpload from 'express-fileupload';
-import { db } from '../database/database.provider';
-import { handleError } from '../../common/responses/error-response';
-import routes from '../../modules/images/images.routes'
+import sequelize from '../database/database.provider';
+import error from '../middleware/error';
+import routes from './routes';
 
-export async function expressApp() {
-  const app = express();
-  await db.useFactory();
-  app.use(fileUpload({
-    limits: { fileSize: 50 * 1024 * 1024 },
-    useTempFiles : true,
-    tempFileDir : '/tmp/'
-  }));
-  app.use(express.json({ limit: '5mb' }));
-  app.use(express.static('download'));
-  app.use(express.static(path.join(__dirname, '../../public')));
-  app.use('/api', routes);
-  app.use(handleError);
-  return app
-}
+const app = express();
+sequelize.sync().then(r => r);
+
+app.use(fileUpload({
+  limits: { fileSize: 50 * 1024 * 1024 },
+}));
+app.use(express.json({ limit: '5mb' }));
+app.use(express.static('download'));
+app.use(express.static(path.join(__dirname, '../../public')));
+
+routes(app);
+
+app.use(error);
+
+export default app
